dde8d094aa5bbba5c78997bbea2cc4289bd1effa
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2019-02-26 22:46:10 -0700

    added routing

--- apps/todos-app/src/app/routing.module.ts
+++ apps/todos-app/src/app/routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Route[] = [
+  {path: '', redirectTo: '/todos', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {path: 'todos', component: TodoComponent},
+  {path: '**', redirectTo: '/todos', pathMatch: 'full'}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class RoutingModule { }
