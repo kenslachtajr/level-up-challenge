95e424c027a0a86608d750b91c1b1a8897647589
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2019-02-26 22:30:46 -0700

    generate todos app componnents and routing module

--- apps/todos-app/src/app/app.module.ts
+++ apps/todos-app/src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreDataModule } from '@workspace/core-data';
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo/todo-details/todo-details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
   imports: [
     BrowserModule, 
-    CoreDataModule],
+    CoreDataModule, 
+    RoutingModule],
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    LoginComponent
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/todos-app/src/app/todo/todo-details/todo-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'workspace-todo-details',
+  templateUrl: './todo-details.component.html',
+  styleUrls: ['./todo-details.component.scss']
+})
+export class TodoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
